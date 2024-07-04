f28c67e7c088dc021bf22a6ccf9d6184bd85e460
Dennis Irsigler <dennis@irsigler.dev>
2024-07-04 13:47:31 +0200

    prepare install.sh

---
+++ src/helm-docs/install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'helm-docs'"
+
+# The 'install.sh' entrypoint script is always executed as the root user.
+#
+# These following environment variables are passed in by the dev container CLI.
+# These may be useful in instances where the context of the final
+# remoteUser or containerUser is useful.
+# For more details, see https://containers.dev/implementors/features#user-env-var
+echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+
+echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+
+# Download the latest x86_64.deb package from GitHub
+curl -s https://api.github.com/repos/norwoodj/helm-docs/releases/latest \
+| grep "browser_download_url.*x86_64.deb" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+
+# Find the downloaded .deb file
+deb_file=$(ls | grep "helm-docs.*x86_64.deb")
+
+# Install the .deb package
+if [ -n "$deb_file" ]; then
+    sudo dpkg -i "$deb_file"
+    # Resolve dependencies if needed
+    sudo apt-get install -f
+else
+    echo "Failed to find the downloaded .deb file."
+    exit 1
+fi
+
+rm -rf /var/lib/apt/lists/*
